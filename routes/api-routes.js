a80e1e9b2190449498d2042cbb9fa0c97a810b9d
Lia OBrien <liaobrien123@gmail.com>
2021-10-30 18:38:14 -0400

    created post route for new exercise in new workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ const db = require('../models');
 // GET last workout
 router.get('/api/workouts', (req, res) => {
       db.Workout.find()
-            .sort({ day: 1 }) // the sort puts the data in reverse chronological order
+            // .sort({ day: 1 }) // the sort puts the data in reverse chronological order
             .then((workout) => {
                   res.status(200).json(workout);
             })
@@ -15,12 +15,22 @@ router.get('/api/workouts', (req, res) => {
             })
 });
 
-// Add exercises to the most recent workout plan. (PUT?)
-// router.put('/api/workouts', (req, res) => {
-//       db.Workout.find()
-// })
 
 // Add new exercises to a new workout plan. (POST)
+router.post('/api/workouts', ({ body }, res) => {
+      db.Workout.create(body)
+            .then((workout) => {
+                  res.status(200).json(workout);
+            })
+            .catch((err) => {
+                  res.status(400).json(err);
+            })
+})
+
+// Add exercises to the most recent workout plan. (PUT?)
+// router.put('/api/workouts/:id', (req, res) => {
+//       db.Workout.find()
+// })
 
 // View the combined weight of multiple exercises from the past seven workouts on the stats page.
 
