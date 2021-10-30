bc9b3bdf4d5d2cfe698aa37e478b2abec88bbcb7
Lia OBrien <liaobrien123@gmail.com>
2021-10-30 13:12:48 -0400

    created basic layout of workout model

--- models/Workout.js
+++ models/Workout.js
@@ -2,3 +2,30 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const WorkoutSchema = new Schema({
+      name: {
+            type: String,
+      },
+      type: {
+            type: String,
+      },
+      weight: {
+            type: Number
+      },
+      sets: {
+            type: Number,
+      },
+      reps: {
+            type: Number,
+      },
+      duration: {
+            type: Number
+      },
+      distance: {
+            type: Number
+      }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
