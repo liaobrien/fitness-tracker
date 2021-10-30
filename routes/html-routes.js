c314746b5501ef173d15227badfcd048b9b8b9b2
Lia OBrien <liaobrien123@gmail.com>
2021-10-30 15:30:48 -0400

    made get route for stats page, required html routes in server.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 // render home page
 router.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname + 'index.html'));
+      res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// render stats page
+router.get('/stats', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/stats.html'))
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,7 +14,9 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./routes/html-routes"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
       useNewUrlParser: true,
