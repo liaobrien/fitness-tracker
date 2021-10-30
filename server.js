b3065b62f48c64c2be2a95a00262cba97f11abf9
Lia OBrien <liaobrien123@gmail.com>
2021-10-30 12:36:46 -0400

    added more boilerplate to server.js

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
+});
+
+app.listen(PORT, () => {
+      console.log(`App running on port ${PORT}!`);
 });
