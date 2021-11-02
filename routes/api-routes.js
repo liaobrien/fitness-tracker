dbd075eaa3402a09c4a17a3023fdad726061564e
Lia OBrien <liaobrien123@gmail.com>
2021-11-02 16:18:39 -0400

    changed sort to chronological order in get workout range route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -36,7 +36,7 @@ router.get('/api/workouts/range', (req, res) => {
                   }
             },
       ])
-            .sort({ _id: -1 }) // reverse chron. order
+            .sort({ _id: 1 }) // chron. order
             .limit(7)
             .then((workout) => {
                   res.status(200).json(workout);
